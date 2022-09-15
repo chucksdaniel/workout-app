bd54836d661bd8befa0f067fa474c93e105b4417
chucks <iamdanielchukwuma@gmail.com>
2022-09-15 13:12:59 +0100

    Update the global state of a user

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,10 +1,18 @@
-import { useReducer } from "react";
+import { useReducer, useEffect } from "react";
 import AuthContext from "./AuthContext";
 import { authReducer } from "./AuthReducer";
 
 export const AuthContextProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(authReducer, { user: null });
 	console.log("AuthContext state", state);
+
+   useEffect(() => {
+      const user = JSON.parse(localStorage.getItem('user'))
+
+      if (user) {
+         dispatch({type: "LOGIN", payload: user})
+      }
+   }, [])
 
 	return (
 		<AuthContext.Provider value={{ ...state, dispatch }}>
